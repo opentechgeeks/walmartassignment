1bb5e07308a894e072b8406469184ce663d16538
ravikanthreddy86@gmail.com <ravikanthreddy86@gmail.com>
2021-01-11 21:30:13 +0100

    initial commit

---
+++ src/main/java/com/walmart/restcontroller/Controller.java
@@ -0,0 +1,13 @@
+package com.walmart.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+ 
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, This is Kranthi";
+    }  
+}
